cd014dfa758ce0522d9dce5096d06f32fa6aa04e
sumaira <sumairafarooq8@gmail.co>
2024-06-12 12:59:00 +0530

    added new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoadingScreenComponent } from './loading-screen/loading-screen.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+
 const routes: Routes = [
   { path: '', component: LoadingScreenComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'home', component: HomeComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' } // Redirect any unknown path to the loading screen
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoadingScreenComponent } from './loading-screen/loading-screen.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoadingScreenComponent,
-    DashboardComponent
+    DashboardComponent,
+    NavbarComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
